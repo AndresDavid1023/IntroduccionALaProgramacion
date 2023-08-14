1b498787df1ebb6b5b72ad520d2521210e343611
AndresDavid1023 <65883240+AndresDavid1023@users.noreply.github.com>
2023-08-14 23:00:05 +0000

    Clase 4

--- main.py
+++ main.py
@@ -62,14 +62,59 @@
 #  print("La suma de todos los numeros enteros desde 1 hasta " + str(n) + "es" + " " + str(suma))
 
 #Ejercicio 2:
-n=0
-m=0
-c=0
-r=0
-n=int(input("Ingrese el dividendo: "))
-m=int(input("Ingrese el divisor: "))
-division=(n//m)
-c=(n//m)
-print(str(n) + " dividido " + str(m) + " da " + str(division) + " y sobra " + str(int(n) % int(m)))
-if c > 1:
-  print
+#n=0
+#m=0
+#c=0
+#r=0
+#n=float(input("Ingrese el dividendo: "))
+#m=float(input("Ingrese el divisor: "))
+#division=(n//m)
+#c=(n//m)
+#print(str(n) + " dividido " + str(m) + " da " + str(division) + " y sobra " + str(float(n) % float(m)))
+#if c > 1:
+  #print("El resultado de la division es mayor a 1")
+#elif c == 1:
+  #print("El resultado de la division es igual a 1")
+#else:
+  #print("El resultado de la division es menor a 1")
+
+#Ejercicio 3:
+#c=0
+#i=0
+#años=0
+#r=0.0
+#c=int(input("¿Cuanto dinero quiere invertir?: "))
+#i=int(input("¿Cuanto es el porcentaje de interes?: "))
+#años=int(input("¿Cuantos años sera la inversion?: "))
+#r=c*((i/100)+1)**años
+#print("La rentabilidad da un total de: " + str(r))
+#if r < 100000:
+  #print("Baja rentabilidad")
+#elif (r > 100000 and r < 1000000):
+  #print("Rentabilidad moderada")
+#else:
+  #print("Es una buena inversion")
+
+#Ejercicio 4:
+#p=0
+#m=0
+#payaso_gr=112
+#muñeca_gr=75
+#p=float(input("¿Cuantos payasos son?: "))
+#m=float(input("¿Cuantas muñecas son?: "))
+#payaso_kg=(payaso_gr*(1/1000)*(p))
+#muñeca_kg=(muñeca_gr*(1/1000)*(m))
+#peso_total=(payaso_kg+muñeca_kg)
+#print("El peso total de la caja es: " + str(peso_total) + "kg")
+#if peso_total > 3000:
+  #envio=input("¿Desea enviarlo?: ")
+  #if envio=="Si":
+    #print("Contenedor enviado")
+  #if envio=="No":
+    #print("Contenedor no enviado")
+
+#Ejercicio 5:
+d=0
+d=float(input("¿Cuanto dinero deposito en la cuenta?: "))
+if d == 0 and d < 1000000:
+  a=(d+)
